105524357544b1e87aac5793d6d16c8f248f404a
Gavin Yip <gavin.yip88@gmail.com>
2024-02-16 22:45:01 +0000

    fetch

---
+++ regexpit/src/app/api/chat-api/route.js
@@ -0,0 +1,3 @@
+export async function POST(request) {
+  return NextResonse.json({ message: "Hello, i work!" });
+}
---
+++ regexpit/src/app/page.js
@@ -0,0 +1,20 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to Regexpit</h1>
+      <button
+        onClick={async () => {
+          const response = await fetch("/api/chat-gpt", {
+            method: "POST",
+            body: JSON.stringify({ message: "Hello, i work!" }),
+          });
+          console.log(response);
+        }}
+      >
+        Generate
+      </button>
+    </main>
+  );
+}
