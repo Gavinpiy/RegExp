a6d35c860bcd8d4ec7b534a8ccd3ddd3dcf60234
Gavin Yip <gavin.yip88@gmail.com>
2024-02-19 15:29:58 +0000

    fetch shoudl be working but openai billing not working

--- regexpit/src/app/api/chat-gpt/route.js
+++ regexpit/src/app/api/chat-gpt/route.js
@@ -1,5 +1,25 @@
 import { NextResponse } from "next/server";
+import { OpenAI } from "openai";
 
 export async function POST(request) {
-  return NextResponse.json({ message: "Hello, i work!" });
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "I will be asking you to give me regular expressions for use in javascript",
+      },
+      { role: "user", content: " how to match a phone number in uk format?" },
+    ],
+    temperature: 0,
+    max_tokens: 1024,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return NextResponse.json(response);
 }
--- regexpit/src/app/page.js
+++ regexpit/src/app/page.js
@@ -3,12 +3,12 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Regexpit</h1>
+      <h1 className="text-4xl font-bold">Welcome to RegExpIt</h1>
       <button
         onClick={async () => {
           const response = await fetch("/api/chat-gpt", {
             method: "POST",
-            body: JSON.stringify({ soneData : true }),
+            body: JSON.stringify({ soneData: true }),
           });
           console.log("response", response);
         }}
