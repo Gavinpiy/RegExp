28545d763adff3a45ab7a639f72b60d9f89ac51b
Gavin Yip <gavin.yip88@gmail.com>
2024-02-20 16:05:46 +0000

    loading

--- regexpit/src/app/components/Prompt.jsx
+++ regexpit/src/app/components/Prompt.jsx
@@ -5,7 +5,7 @@ export default function Prompt({onSubmit, loading}) {
   const [prompt, setPrompt] = useState("");
   return (
     <form
-      className=" flex-col p-2 block mx-auto border-2 border-black rounded-lg bg-gray-200"
+      className=" flex-col p-2 block mx-auto w-full  border-2 border-black rounded-lg bg-gray-200"
       onSubmit={(e) => {
         e.preventDefault();
 
@@ -16,15 +16,19 @@ export default function Prompt({onSubmit, loading}) {
         setPrompt("");
       }}
     >
-      <label>Questions</label>
+      <label>What Regular Expression do you need?</label>
       <input
+      className="border-2 border-black rounded-lg h-20 w-full flex  overflow-y-auto p-2 my-2"
         type="text"
         value={prompt}
         onChange={(e) => {
           setPrompt(e.target.value);
         }}
       />
-      <input type="submit" disabled={loading}/>
+      <div className="flex gap-2 items-center"><input type="submit" disabled={loading}
+      className="bg-blue-200 rounded-sm my-1 flex justify-end"/>
+      <p className={loading ? "block" : "hidden"} > Loading...</p></div>
+      
     </form>
   );
 }
--- regexpit/src/app/page.js
+++ regexpit/src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start gap-20 p-20 lg:p-36 ">
       <h1 className="text-4xl font-bold">Welcome to RegExpIt</h1>
       <Prompt
         loading={loading}
@@ -43,7 +43,7 @@ export default function Home() {
       <div>
         {choices.map((choice) => (
           <div
-            className="border-solid border-black border-2 rounded-lg"
+            className="border-solid border-black border-2 rounded-lg p-2 my-2 w-96 overflow-y-auto h-48"
             key={choice.index}
           >
             {choice.message.content}
